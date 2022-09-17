b81485850128f7a2607d154feb736f4c5a49a642
Ulises <ulipalazzoo@gmail.com>
2022-09-17 18:20:12 -0300

    verificador de edad

--- tp-entrega/js/script.js
+++ tp-entrega/js/script.js
@@ -1,1 +1,35 @@
-document.querySelector()
+"use strict";
+
+let es_mayor = false;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const btnVerif = document.querySelector("#btn-verif");
+    const popup = document.querySelector(".popup");
+    const altoAhi = document.querySelector(".verificador img");
+    
+    btnVerif.addEventListener("click",(e)=>{
+        if(retornarEdad(e.target.previousElementSibling.value) < 18)
+            window.open("https://www.danonino.com.ar/");
+        else{
+            es_mayor = true;
+            popup.classList.add("desaparecer");
+            altoAhi.classList.add("desaparecer-translate");
+            setTimeout(()=>{
+                document.querySelector(".verificador").remove();
+            }, 1000);
+            document.querySelector(".container").classList.remove("blur");
+            document.querySelector(".head-nav").classList.remove("blur");
+        }
+    });
+});
+
+function retornarEdad(fecha){
+    let hoy = new Date();
+    let cumple = new Date(fecha);
+    let edad = hoy.getFullYear() - cumple.getFullYear();
+    let mes = hoy.getMonth() - cumple.getMonth();
+    if(mes < 0 || (mes === 0 && hoy.getDate() < cumple.getDate())){
+        edad--;
+    }
+    return edad;
+}
