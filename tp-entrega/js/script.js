33bb339716b61eebc29e0c8f4d4ae21efbe38a45
Ulises <ulipalazzoo@gmail.com>
2022-10-16 21:59:02 -0300

    arreglo el verificador y acomodo las carpetas

--- tp-entrega/js/script.js
+++ tp-entrega/js/script.js
@@ -1,27 +1,9 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded",()=>{
-    const btnVerif = document.querySelector("#btn-verif");
-    const popup = document.querySelector(".popup");
-    const altoAhi = document.querySelector(".verificador img");
     const contador = document.querySelector("#odometer");
     odometer(contador);
 
-    //darAcceso();
-    btnVerif.addEventListener("click",(e)=>{
-        let fecha = e.target.previousElementSibling.value;
-        console.log(retornarEdad(fecha));
-        if(retornarEdad(fecha) < 18 && (retornarEdad(fecha)!==""))
-            window.open("https://www.danonino.com.ar/");
-        else{
-            popup.classList.add("desaparecer");
-            altoAhi.classList.add("desaparecer-translate");
-            setTimeout(()=>{
-                document.querySelector(".verificador").remove();
-            }, 1000);
-            document.querySelector(".container").classList.remove("blur");
-            document.querySelector(".head-nav").classList.remove("blur");
-        }
     });
     /*
     function darAcceso(){
@@ -34,7 +16,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         document.querySelector(".head-nav").classList.remove("blur");
     }
     */
-});
 
 function odometer(elem){
     let inicio = Math.floor(Math.random()*10000)+1000;
@@ -44,15 +25,3 @@ function odometer(elem){
         elem.innerHTML = inicio;
     }, 5000);
 }
-
-function retornarEdad(fecha){
-    let hoy = new Date();
-    let cumple = new Date(fecha);
-    let edad = hoy.getFullYear() - cumple.getFullYear();
-    let mes = hoy.getMonth() - cumple.getMonth();
-    if(mes < 0 || (mes === 0 && hoy.getDate() < cumple.getDate())){
-        edad--;
-    }
-    return edad;
-}
-
