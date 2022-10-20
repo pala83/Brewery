877a2dd60fabec9f9b143e613879d3982fcc87d1
Ulises <ulipalazzoo@gmail.com>
2022-10-20 01:05:02 -0300

    trabajo terminado, falta hacer lo responsive

--- tp-entrega/js/script.js
+++ tp-entrega/js/script.js
@@ -47,12 +47,44 @@ document.addEventListener("DOMContentLoaded",()=>{
             });
             break;
         case "contacto":
-            console.log("aca llega");
+            let operaciones = ["+", "-", "*"];
+            let cuenta = (Math.floor(Math.random()*100)+1)+operaciones[Math.floor(Math.random()*operaciones.length)]+(Math.floor(Math.random()*100)+1);
+            const captcha = document.querySelector("#suma-captcha");
+            captcha.innerHTML = cuenta;
             const formContacto = document.querySelector("#form-contacto");
             formContacto.addEventListener("submit",(e)=>{
                 e.preventDefault();
                 let datos = getData(formContacto);
-                console.log(datos);
+                const msgError = document.querySelector("#error");
+                console.log(cuenta);
+                if(!datos.gusta){
+                    msgError.innerHTML = "Si no te gusta la cerveza entonces no nos interesa su contacto, que tenga un buen dia"
+                    msgError.classList.remove("message-success");
+                    msgError.classList.add("message-error");
+                }
+                else{
+                    if(datos.nombre && datos.apellido && datos.email && datos.pais && datos.ciudad && datos.consulta && datos.resultado){
+                        console.log(datos.resultado);
+                        if(datos.resultado == eval(cuenta)){
+                            let form = new FormData(formContacto);
+                            enviarMail(formContacto, form);
+                            formContacto.reset();
+                            msgError.innerHTML = "Gracias por contactarte con nosotros, contestaremos lo antes posible";
+                            msgError.classList.remove("message-error");
+                            msgError.classList.add("message-success");
+                        }
+                        else{
+                            msgError.innerHTML = "El captcha esta mal resuelto!";
+                            msgError.classList.remove("message-success");
+                            msgError.classList.add("message-error");    
+                        }
+                    }
+                    else{
+                        msgError.innerHTML = "Algo no salio bien, no intentes romper el formulario por favor";
+                        msgError.classList.remove("message-success");
+                        msgError.classList.add("message-error");
+                    }
+                }
             })
             break;
         default:
@@ -61,6 +93,25 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 });
+
+async function enviarMail(form, data){
+    try{
+        console.log(form.action, form.method)
+        let respuesta = await fetch(form.action, {
+                                    method: form.method,
+                                    body: data,
+                                    headers: {
+                                        'Accept': 'application/json'
+                                    }
+        })
+        if(respuesta.ok){
+            console.log("mensaje enviado");
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
 
 function getData(formulario){
     let form = new FormData(formulario);
